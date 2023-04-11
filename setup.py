a869e00362635235255ffffc812b855cabecbdff
Fabian Isensee <f.isensee@dkfz.de>
2023-04-11 17:58:44 +0200

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='kits23',
     packages=find_namespace_packages(include=["kits23*"]),
-    version='0.1.0',
+    version='0.1.1',
     description='',
     zip_safe=False,
     install_requires=[
